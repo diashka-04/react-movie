f30e675119ff772874c3d2ece6c17e27c10b30ae
diashka-04 <119756677+diashka-04@users.noreply.github.com>
2023-04-03 19:24:18 +0300

    создание футера сайта

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Header} from './layout/Header'
+import {Footer} from './layout/Footer'
+
 
 function App() {
   return (
     <>
      < Header />
+     <Footer />
     </>
   );
 }
